88837958973a042c11e032974f321488a7e44c1a
hihassan1998 <hassanihussain1998@gmail.com>
2024-02-29 13:52:14 -0500

    Merge remote-tracking branch 'origin/master'

--- settings.py
+++ settings.py
@@ -4,14 +4,22 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
+<<<<<<< HEAD
         'PASSWORD': 'MTU3NjQtaGFzc2Fu',
+=======
+        'PASSWORD': 'MzMxMi1oYXNzYW5p',
+>>>>>>> 47a491c (Query Spanning Relationships checking from cloud ide)
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
 
 INSTALLED_APPS = (
+<<<<<<< HEAD
     'crud',
+=======
+    'related_objects',
+>>>>>>> 47a491c (Query Spanning Relationships checking from cloud ide)
 )
 
 SECRET_KEY = 'SECRET KEY for this Django Project'
